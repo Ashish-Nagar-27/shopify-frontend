52a8209af2d6741fe165fb5f94663a8a9e6667bd
Ashish Nagar <ashish@trackocity.io>
2026-07-22 18:29:42 +0530

    fix: default theme update

--- src/store/useThemeStore.ts
+++ src/store/useThemeStore.ts
@@ -11,7 +11,7 @@ interface ThemeState {
 export const useThemeStore = create<ThemeState>()(
     persist(
         (set) => ({
-            mode: "dark",
+            mode: "dim",
             setMode: (mode) => set({ mode }),
         }),
         { name: "theme-storage" }
